06b6cda44e3699507c5e4d750864c2f9a9a88f27
J-Kaun <kangeun9727@gmail.com>
2024-06-17 20:25:45 +0900

    [STOP]
    파티클 시스템을 위한 게임 매니저 생성

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public  static GameManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    public ParticleSystem EffectParticle;
+
+    private void Awake()
     {
-        
+        Instance = this;
+
+        EffectParticle = GameObject.FindGameObjectWithTag("Particle").GetComponent<ParticleSystem>();
     }
 }
